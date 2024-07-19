ec8be8f6b28b51a849237280f6a7b75acfa12445
devklick <devklick@gmail.com>
2024-07-19 22:14:46 +0100

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://devklick.github.io/u-he-skins-ui/",
+  base: "https://devklick.github.io/u-he-skins/",
 });
